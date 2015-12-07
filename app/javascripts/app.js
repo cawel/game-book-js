b74ff371d4cc94233040c4da22130cc1b389bac5
Martin Carel <martin.carel@gmail.com>
2015-12-07 11:25:18 -0500

    app: encapsulate story-related code into a module

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,38 +1,74 @@
 $(function() {
   console.log("firing document ready event!");
 
-  var current_story;
-
-  var show_next_chapter = function(chapter_nb){
-    $.getJSON('/stories/' + current_story + '/' + chapter_nb + '.js', function(result){
-      var current_chapter = result;
-      $('.chapter > .title').html(current_chapter.title);
-      $('.chapter > .text').html(current_chapter.text);
-
-      $('.chapter > .choices').empty();
-      current_chapter.choices.forEach(function(choice){
-        $('.chapter > .choices').append("<ul>");
-        $('.chapter > .choices').append("<li><a data-next-chapter-nb='" + choice[1] + "' class='choice' href='#'>" + choice[0] + "</a></li>");
-        $('.chapter > .choices').append("</ul>");
-      });
-
-      // edge case for the last chapter
-      if(current_chapter.choices.length == 0){
-        $('.choices-title').text('The End.');
-      }
-    });
-  };
-
-
-  $(document).on('click', 'a.choice', function(el){
-    next_chapter_nb = $(el.target).data('next-chapter-nb');
-    show_next_chapter(next_chapter_nb);
+  // load a new story upon user selection
+  $('.main').on('click', '.home a', function(el){
+    var story_title = $(el.target).text().toLowerCase().replace(' ', '-');
+    var story = Story(story_title, '.main');
+    story.start();
   });
 
-  $(document).on('click', '.home a', function(el){
-    current_story = $(el.target).text().toLowerCase().replace(' ', '-');
-    $('.main').load('story.html', function(){
-      show_next_chapter(1);
-    });
+  $(document).ajaxError(function (e, xhr, settings, error) {
+    console.log(error);
   });
 });
+
+
+function Story(story_title, dom_container){
+
+  var chapters = [];
+  var story_endpoint_url_base = '/stories/' + story_title + '/';
+
+  var start = function(){
+    $(dom_container).load('story.html', function(){
+      load_next_chapter(1);
+    });
+  }
+
+  var story_endpoint_url = function(chapter_nb){
+    return story_endpoint_url_base + chapter_nb + '.js';
+  }
+
+  var display_next_chapter = function(chapter){
+    $('.chapter > .title').html(chapter.title);
+    $('.chapter > .text').html(chapter.text);
+
+    $('.chapter > .choices').empty();
+    chapter.choices.forEach(function(choice){
+      $('.chapter > .choices').append("<ul>");
+      $('.chapter > .choices').append("<li><a data-next-chapter-nb='" + choice[1] + "' class='choice' href='#'>" + choice[0] + "</a></li>");
+      $('.chapter > .choices').append("</ul>");
+      preload_chapters(choice[1]);
+    });
+
+    // edge case for the last chapter
+    if(chapter.choices.length == 0){
+      $('.choices-title').text('The End.');
+    }
+  }
+
+  var load_next_chapter = function(chapter_nb){
+    if (chapters[chapter_nb] ) {
+      display_next_chapter(chapters[chapter_nb])
+    } else { 
+      $.getJSON(story_endpoint_url(chapter_nb), function(chapter){
+        display_next_chapter(chapter);
+      });
+    }
+  };
+
+  var preload_chapters = function(chapter_nb){
+    $.getJSON(story_endpoint_url(chapter_nb), function(chapter){
+      chapters[chapter_nb] = chapter;
+    });
+  }
+
+  $(dom_container).on('click', 'a.choice', function(el){
+    next_chapter_nb = $(el.target).data('next-chapter-nb');
+    load_next_chapter(next_chapter_nb);
+  });
+
+  return {
+    start : start
+  }
+}
