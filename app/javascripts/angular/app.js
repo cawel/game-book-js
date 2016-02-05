5c09b647771a3562cb7ccf74d38a07c898eba6ff
Martin Carel <martin.carel@gmail.com>
2016-02-04 21:43:22 -0500

    angular: support chapters preloading

--- app/javascripts/angular/app.js
+++ app/javascripts/angular/app.js
@@ -4,24 +4,40 @@
   app.controller('StoryController', function($scope, $compile){
     var url = 'https://game-book-stories.firebaseio.com/stories/' + $.selected_story + '/chapters'
     var storiesDB = new Firebase(url);
+    var chapters = [];
 
     var fetchFromFirebase = function(chapter_nb, callback){
       storiesDB.child(String(chapter_nb)).on("value", function(snapshot){
+        console.log('Fetching chapter ' + chapter_nb + ' from remote.');
         var chapter = snapshot.val();
-        if( chapter == null ){
-          console.log("Could not find the chapter '" + chapter_nb + "'");
+        if(chapter){
+          chapters[chapter_nb] = chapter;
+          if(callback) callback(chapter);
         }else{
-          callback(chapter);
+          console.log("Could not find the chapter '" + chapter_nb + "'");
         }
       });
     };
 
-    $scope.selectChapter = function(chapter_nb){
-      fetchFromFirebase(chapter_nb, function(chapter){
-        $scope.chapter = chapter;
-        var el = $compile("<chapter chapter='chapter' select-chapter='selectChapter'></chapter>")($scope);
-        $(".chapter-container").html( el );
+    var showChapter = function(chapter){
+      $scope.chapter = chapter;
+      var el = $compile("<chapter chapter='chapter' select-chapter='selectChapter'></chapter>")($scope);
+      $(".chapter-container").html( el );
+
+      // preload chapters
+      angular.forEach($scope.chapter['choices'], function(choice){
+        fetchFromFirebase(choice[1]);
       });
+    };
+
+    $scope.selectChapter = function(chapter_nb){
+      var chapter = chapters[chapter_nb];
+      if (chapter){
+        console.log('Reading chapter ' + chapter_nb + ' from cache.');
+        showChapter(chapter);
+      }else {
+        fetchFromFirebase(chapter_nb, showChapter);
+      }
     };
 
     // start at the first chapter
